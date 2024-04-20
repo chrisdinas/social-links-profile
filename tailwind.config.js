9ffef96a5c22040630faf5bfe88962aaa6a49868
chrisdinas <christiandinasdev@gmail.com>
2024-04-20 10:26:21 -0400

    Commit message

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {
       colors: {
         "primary-color": "var(--primary-color)",
+        grey: "var(--grey)",
+        darkGrey: "var(--darkGrey)",
+        offBlack: "var(--offBlack)",
       },
     },
   },
